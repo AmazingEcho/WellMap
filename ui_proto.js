060605c52636b8d03cd3c5dcc9204e9c2d447b6d
Thomas Condon <tconxi@gmail.com>
2014-10-26 21:44:52 -0600

    Updating - gmaps.js

--- ui_proto.js
+++ ui_proto.js
@@ -1,18 +1,34 @@
 // JavaScript Document
+// ui_proto.js
+// Author: Thomas 'Toss' Condon
 
+// Small collection javascript to power current UI Prototype
+
+
+var map;
 $('document').ready(function(){
-    
+/*    
 function initialize()
 {
 var mapProp = {
   center:new google.maps.LatLng(55.00,-115.00),
   zoom:5,
-  mapTypeId:google.maps.MapTypeId.ROADMAP
+  mapTypeId:google.maps.MapTypeId.TERRAIN,
+  disableDefaultUI:true
   };
 var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
 	}
 
 google.maps.event.addDomListener(window, 'load', initialize);
+*/
+map = new GMaps({
+	div: '#googleMap',
+	lat: 55.00,
+	lng: -115.00,
+	zoom: 5,
+	mapTypeId:google.maps.MapTypeId.TERRAIN,
+	disableDefaultUI:true
+	});
 
 $(function(){
 	$( "#slider" ).slider();
@@ -38,4 +54,15 @@ $('#sidebar-toggle').click(function() {
 	console.log("I hear you!");
 });
 
+var path = [[55.000,-115.000],[55.000,-113.000],[52.000,-113.000],[52.000,-115.000]];
+
+polygon = map.drawPolygon({
+  paths: path, // pre-defined polygon shape
+  strokeColor: '#BBD8E9',
+  strokeOpacity: 1,
+  strokeWeight: 3,
+  fillColor: '#BBD8E9',
+  fillOpacity: 0.6
 });
+
+});	// End of $('document').ready(function());
