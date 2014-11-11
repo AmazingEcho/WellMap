2f495f5488c10b03ea1a2bd3297d62f6844ba3df
Thomas Condon <tconxi@gmail.com>
2014-11-10 21:03:12 -0700

    Controller Test
    Testing Linked Lists with JSClass

--- Controller_Test.js
+++ Controller_Test.js
@@ -1,38 +1,61 @@
-// JavaScript Document
+/*
+Controller_Test.js
+Author: Thomas Condon
+Original: Nov 9th 2014
+
+Experimenting with data structures in Javascript
+
+Using jsclass so I don't have to code data structures that I've coded in C++/Java a dozen times for other courses...
+http://jsclass.jcoglan.com/
+https://github.com/jcoglan/jsclass
+*/
+
+JS.require('JS.LinkedList', function(LinkedList){
+
+console.log("Running Controller_Test.js!");
 
 function Controller(){
-	this.mapList = MapList();
+	console.log("Initializing Controller");
+	this.mapList = new MapList();
 }
 
-// MapList is a linked list of maps
-// Oh boy, linked lists in Javascript!
-// Now that I think about it, I'm going to be using a lot of linked lists...
-// I wonder if theres any generic type LL tools I can use...
+// MapList uses a linked list of maps
+// LinkedList provided by jsclass
 function MapList(){
+	console.log("Initializing MapList");
+	this.mapsLL = new LinkedList.Doubly.Circular();
+	this.size = 0;
+	this.active = null;
 	
-	this.listHead = null;
-	this.listTail = null;
-	this.length = 0;
-	
-	this.newMap = function(){
-		if(this.listHead = null){
-			this.listHead = new MapNode(length);
-			this.listTail = this.listHead;
+	this.createMap = function(name){
+		var newMap = new Map(name);
+		this.mapsLL.push(newMap)
+		this.size++;
 		}
-		else{
-			this.listTail.next = new MapNode(length);
-			this.listTail = this.listTail.next;
-		}
-		length++;
-	}
-}
-
-function MapNode(index_p){
-	this.index = index_p;
-	this.next = null;
-	this.map = new Map();
+	//Write these later
+	this.deleteMap = function(index){}
+	this.switchMap = function(index){}
 }
 
 // Map will contain one instance of MetaData, and then however many instances of layers
-function Map(){
+function Map(name){
+	console.log("Creating map: " + name)
+	this.metdata = new MapMetadata(name);
 }
+
+function MapMetadata(name){
+	this.mapName = name;
+}
+
+console.log("Initializing");
+var The_Controller = new Controller();
+
+The_Controller.mapList.createMap("TestMap01");
+The_Controller.mapList.createMap("TestMap02");
+
+console.log("Map list size is " + The_Controller.mapList.size)
+console.log("This should be 2");
+
+console.log("Initialization complete!");
+
+});
