97df68b59868af5fb3d253622c7db976ab6c6197
Thomas Condon <tconxi@gmail.com>
2014-11-12 00:28:59 -0700

    Controller

--- Controller_Test.js
+++ Controller_Test.js
@@ -20,9 +20,9 @@ function Controller(){
 	
 	this.createMap = function(name){
 		this.mapList.createMap(name);
-	}
+		}
 	this.switchMap - function(){
-	}
+		}
 }
 
 // MapList uses a linked list of maps
@@ -33,18 +33,16 @@ function MapList(){
 	this.size = 0;
 	this.active = null;
 	
+	this.switchMap = function(index){
+		this.active = this.mapsLL.at(index);
+		}
+
 	this.createMap = function(name){
 		var newMap = new Map(name);
 		this.mapsLL.push(newMap);
 		// A newly created map should be the active map
-		
-		this.size++;
+		this.active = this.size++;
 		}
-	
-	this.switchMap = function(index){
-		this.active = mapLL.at(index);
-		}
-		
 	//Write these later
 	this.deleteMap = function(index){}
 }
@@ -52,7 +50,7 @@ function MapList(){
 // Map will contain one instance of MetaData, and then however many instances of layers
 function Map(name){
 	console.log("Creating map: " + name)
-	this.metdata = new MapMetadata(name);
+	this.metadata = new MapMetadata(name);
 }
 
 function MapMetadata(name){
@@ -66,7 +64,12 @@ The_Controller.createMap("TestMap00");
 The_Controller.createMap("TestMap01");
 
 console.log("Map list size is " + The_Controller.mapList.size + ".  This should be 2");
-console.log("Active Map is " + The_Controller.mapList.active);
+console.log("Active Map is " + The_Controller.mapList.mapsLL.at(The_Controller.mapList.active).metadata.mapName + ".  This should be 1");
+console.log("Switching Active Map!");
+
+The_Controller.mapList.switchMap(0);
+
+console.log("Active Map is " + The_Controller.mapList.mapsLL.at(The_Controller.mapList.active).metadata.mapName + ".  This should be 0");
 
 console.log("Initialization complete!");
 
