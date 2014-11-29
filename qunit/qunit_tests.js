a9beda5b93220b90f1cbf1e4ab0cfe2600ca5d25
Thomas Condon <tconxi@gmail.com>
2014-11-28 22:27:57 -0700

    Update qunit_tests.js

--- qunit/qunit_tests.js
+++ qunit/qunit_tests.js
@@ -45,3 +45,24 @@ test("Point Layer Creation",function(){
 	the_controller.the_map.layers[0].points[0].lat === 55.0 &&
 	the_controller.the_map.layers[0].points[0].long === -115.0, "Point created");
 });
+
+test("Path Layer Creation",function(){
+	var the_controller = new controller();
+	the_controller.newPathLayer("Test Path Layer")
+	var testPath[0] = new Point(55.0, -115.0);
+	testPath[1] = new Point(54.0, -114.0);
+	testPoint[2] = new Point(55.0, -113.0);
+	testPoint[3] = new Point(56.0, -112.0);
+	the_controller.the_map.layers[0].addPath("Test Path", "Road", testPath);
+	
+	ok(
+	the_controller.the_map.layers[0].paths[0].points[0].lat === 55.0 &&
+	the_controller.the_map.layers[0].paths[0].points[0].long === -115.0 &&
+	the_controller.the_map.layers[0].paths[0].points[1].lat === 54.0 &&
+	the_controller.the_map.layers[0].paths[0].points[1].long === -114.0 &&
+	the_controller.the_map.layers[0].paths[0].points[2].lat === 55.0 &&
+	the_controller.the_map.layers[0].paths[0].points[2].long === -113.0 &&
+	the_controller.the_map.layers[0].paths[0].points[3].lat === 56.0 &&
+	the_controller.the_map.layers[0].paths[0].points[3].long === -112.0
+	, "Point created");
+});
