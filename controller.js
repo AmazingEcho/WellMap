8c0307e476b2f067b9cafaa47f20f93e509c53dc
Thomas Condon <tconxi@gmail.com>
2014-11-29 23:59:26 -0700

    More Controller

--- controller.js
+++ controller.js
@@ -9,6 +9,10 @@ function controller(){
 	this.newLayer = function(name){
 		this.the_map.newLayer(name);
 	};
+	
+	this.newPointLayer = function(name){
+		this.the_map.newPointLayer(name);
+	}
 }
 
 function map(){
@@ -16,6 +20,10 @@ function map(){
 	
 	this.newLayer = function(name){
 		this.layers.push(new layer(name));
+	};
+	
+	this.newPointLayer = function(name){
+		this.layers.push(new pointLayer(name));
 	};
 	
 	this.layerCount = function(){
@@ -27,3 +35,33 @@ function layer(name){
 	this.name = name;
 }
 
+function GMapPoint(lat, long){
+	this.lat = lat;
+	this.long = long;
+}
+
+function Point(name, type, gmpoint){
+	this.name = name;
+	this.type = type;
+	this.point = gmpoint;
+}
+
+function pointLayer(name){
+	
+	this.inheritFrom = layer;
+	this.inheritFrom();
+	
+	this.points = [];
+	
+	this.addPoint = function(name, type, gmpoint){
+		this.points.push(new Point(name, type, gmpoint));
+	};
+	
+	this.getLat = function(index){
+		return this.points[index].point.lat;
+	};
+	
+	this.getLong = function(index){
+		return this.points[index].point.long;
+	};
+}
--- qunit/qunit_tests.js
+++ qunit/qunit_tests.js
@@ -42,14 +42,15 @@ test("Point Layer Creation",function(){
 	the_controller.the_map.layers[0].addPoint("Test Point", "Well", testPoint);
 	
 	ok(
-	the_controller.the_map.layers[0].points[0].lat === 55.0 &&
-	the_controller.the_map.layers[0].points[0].long === -115.0, "Point created");
+	the_controller.the_map.layers[0].getLat(0) === 55.0 &&
+	the_controller.the_map.layers[0].getLong(0) === -115.0, "Point created");
 });
 
 test("Path Layer Creation",function(){
 	var the_controller = new controller();
-	the_controller.newPathLayer("Test Path Layer")
-	var testPath[0] = new Point(55.0, -115.0);
+	the_controller.newPathLayer("Test Path Layer");
+	var testPath = new Array();
+	testPath[0] = new Point(55.0, -115.0);
 	testPath[1] = new Point(54.0, -114.0);
 	testPoint[2] = new Point(55.0, -113.0);
 	testPoint[3] = new Point(56.0, -112.0);
