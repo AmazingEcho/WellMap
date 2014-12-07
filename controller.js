76b229ad21ac86d3ba41423aa6015c20ae72fd84
Thomas Condon <tconxi@gmail.com>
2014-12-06 23:48:06 -0700

    Controller Updates
    HAHA!  I broke everything in the controller because inheritance in JS
    works NOTHING like other OO-languages!  This is why we have a test
    suite!  Whatever!  I'll try fixing it next week or something!

--- controller.js
+++ controller.js
@@ -3,10 +3,29 @@ WellMap
 controller.js
 */
 
+/*
+
+Constructors for the various classes.
+
+*/
+
+/*
+controller
+The controller that handles all of the control logic
+*/
 function controller(){
+	
+	// Classes will have attributes and functions, both of which MUST be prefaced with a 'this.'
+	// When the controller is made, the first thing it does is create a map object
+	// It does this by calling the map() constructor function
 	this.the_map = new map();
 	
+	// Functions are made in a similar fashion as attributes
+	// function() means that that we're declaring a function (duh)
+	// the name in function(name) is an incoming argument
+	// Notice how its not 'String name'.  That's because JavaScript uses Duck Typing.  Look it up...
 	this.newLayer = function(name){
+		// the only thing this function does is call the newLayer() function on the_map object, passing along the name...
 		this.the_map.newLayer(name);
 	};
 	
@@ -16,10 +35,13 @@ function controller(){
 }
 
 function map(){
+	// Arrays in Javascript aren't like Arrays in C/C++
+	// You don't have to specify how big the array is or what it's storing
 	this.layers = new Array();
 	
 	this.newLayer = function(name){
-		this.layers.push(new layer(name));
+		// You can put new objects (or anything) into an Array with the .push() method
+		this.layers.push(new Layer(name));
 	};
 	
 	this.newPointLayer = function(name){
@@ -31,9 +53,20 @@ function map(){
 	}
 }
 
-function layer(name){
-	this.name = name;
-}
+var Layer = Class.extend({
+	constuct: function(nameA){
+		this.name = nameA;
+		this.visible = true;
+	},
+	
+	getName: function(){
+		return this.name;
+	},
+	
+	getVis: function(){
+		return this.visible;
+	}
+});
 
 function GMapPoint(lat, long){
 	this.lat = lat;
@@ -46,22 +79,17 @@ function Point(name, type, gmpoint){
 	this.point = gmpoint;
 }
 
-function pointLayer(name){
+var pointLayer = Layer.extend({
 	
-	this.inheritFrom = layer;
-	this.inheritFrom();
-	
-	this.points = [];
-	
-	this.addPoint = function(name, type, gmpoint){
+	addPoint: function(name, type, gmpoint){
 		this.points.push(new Point(name, type, gmpoint));
-	};
+	},
 	
-	this.getLat = function(index){
+	getLat: function(index){
 		return this.points[index].point.lat;
-	};
+	},
 	
-	this.getLong = function(index){
+	getLong: function(index){
 		return this.points[index].point.long;
-	};
-}
+	}
+});
