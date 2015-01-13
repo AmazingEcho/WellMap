5f6f3bd0941992f14b1badaad6920c1e97fea17a
Thomas Condon <tconxi@gmail.com>
2015-01-12 22:34:19 -0700

    Semantics Fixed
    Turns out they just changed the tags on a few things.

--- ui.js
+++ ui.js
@@ -6,42 +6,62 @@
 
 $('document').ready(function(){
 	
+	// I used to have the controller initialize GMaps on it's own when the constructor was called
+	// This caused the QUnit Test Suite to go all read, so I opted instead to have a seperate method
 	var the_controller = new controller();
 	the_controller.initGMaps();
 	
-	// TODO: Start up menu option to select between starting a new map and loading one
-	// Also, must be unclosable, in order to force a choice...
+	// The first thing the application does on startup is show the startup modal
 	console.log("Ititializing Start Modal");
-	$('.basic.modal.startup')
+	$('.ui.modal.startup')
 		.modal('setting', 'closable', false)
 		.modal('show');
 	
+	// This is the button that starts up a new map
 	$('.massive.ui.button.startupNewMap').click(function(){
-		// TODO: control logic that either starts a new map or tries to load an existing one, based on user input...
 		the_controller.newMap();
 		console.log("Controller status: On - Map name: " + the_controller.the_map.metadata.mapName);
-		$('.basic.modal.startup').modal('hide');
+		$('.ui.modal.startup').modal('hide');
 		// maybe bring up a new map info modal?
 	})
 	
+	// This is the button that loads a new map
+	// Since map loading hasn't been done yet, it simply issues an alert
 	$('.massive.ui.button.startupLoadMap').click(function(){
 		// TODO: hook this up to the map load function
 		alert("Map Saving/Loading not yet implemented...");
 	})
 
+// Sidebar options
+// This one simply adds some properties to the sidebar
+// transition: "overlay" means the sidebar acts as an overlay, rather than a 'pusher'
+// dimPage means the rest of the page gets dimmed when the sidebar is open
+// closable means the sidebar closes when you click outside of of it
+$('.ui.sidebar').sidebar({
+	// overlay: true,
+	// overlay got depritiated!
+	transition: "overlay",
+	dimPage: false,
+	closable: false
+	});
+
+// And, of course, clicking the sidebar toggle button makes the sidebar open and close!
 $('#sidebar-toggle').click(function() {
 	//$('#mapArea').toggleClass('slide-away');
-	$('.ui.sidebar')
-		.sidebar({overlay: true})
-		.sidebar('toggle');
+	$('.ui.sidebar').sidebar('toggle');
 });
+
+$('.ui.accordion').accordion({
+	exclusive: false
+	});
+
+$('.menu .item').tab();
 
 $('.ui.dropdown').dropdown();
 
 
-
 $('#modal-button-importPrivateDB').click(function(){
-	$('.basic.modal.importPrivateDB').modal('show');
+	$('.ui.modal.importPrivateDB').modal('show');
 });
 
 $('.ui.button.randomPoints').click(function(){
