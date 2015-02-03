44fa770d3f9d402854facc4a47a006a12a2027e5
Thomas Condon <tconxi@gmail.com>
2015-02-02 23:03:45 -0700

    Update Default.aspx.cs

--- qunit/Default.aspx.cs
+++ qunit/Default.aspx.cs
@@ -1,5 +1,22 @@
-[WebMethod]                                 //Default.aspx.cs
-  public static int csharp_test()
-  {
-     return 2;
-  }
+protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.QueryString["Method"] == "GetServerDate")
+            {
+                Response.Cache.SetCacheability(HttpCacheability.NoCache);
+                GetServerDate(Request.QueryString["format"]);
+            }
+        }
+ 
+        private void GetServerDate(string dateformat)
+        {
+            Response.Clear();
+            if (dateformat.Equals("utc"))
+            {
+                Response.Write(DateTime.Now.ToUniversalTime().ToString());
+            }
+            else
+            {
+                Response.Write(DateTime.Now.ToLocalTime().ToString());
+            }
+            Response.End();
+        }
