4f208eec380e77f28a5a103ec9289a7fd4f339ff
pkusoglu <pkusoglu@gmail.com>
2015-02-07 11:51:03 -0700

    Update ui.js

--- ui.js
+++ ui.js
@@ -87,7 +87,7 @@ $('#modal-button-settingsButtonOkay').click(function(){
 
 $('#modal-button-saveMapButton').click(function(){ 
 	$('.ui.modal.settings').modal('show');
-	alert("save!")
+	alert("save!");
 });
 
 $('#modal-button-search').click(function(){
