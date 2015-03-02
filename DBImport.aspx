c15fe8e9aca87c95b79b29a0e03d71e850895696
dtfsheedy <dtfsheedy@gmail.com>
2015-03-02 13:55:28 -0700

    dbimport which is actually db add

--- DBImport.aspx
+++ DBImport.aspx
@@ -1,4 +1,4 @@
-<%@ Page Language="C#" AutoEventWireup="true" CodeFile="DBImport.aspx.cs" Inherits="_Default" %>
+<%@ Page Language="C#" AutoEventWireup="true" CodeFile="DBImport.aspx.cs" Inherits="_DBImport" %>
  
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -9,28 +9,67 @@
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/jquery-ui.min.js"></script>
        <script type="text/javascript">
-        $(document).ready(function () {
-		$.ajax({
-			type: "POST",
-			url: 'WellMap.DBImport.aspx/delete_well',
-			data: "{workingWellKey: " +key+ "}",
-			contentType: "application/json; charset=utf-8",
-			datatype: "json",
-			async:false,
-			success: function(msg){
-				$('#toDeleteWellKey').val('');
-				$("#divResult").html("success");
-			},
-			error: function(e){
-				$("#divResult").html("Something Wrong.");
-			}
-		});
+           $(document).ready(function () {
+               $('#Button1').click(function () {
+                   $.ajax({
+                       type: "POST",
+                       url: 'WellMap.dbaccess/add_new',
+                       data: "{'wellGroup': " +document.getElementById('txtWellGroup').value+ ", 'wellName': " +document.getElementById('txtWellName').value+ ", 'wellType': " +document.getElementById('txtWellType').value+ ", 'latitude': " +document.getElementById('txtWellLat').value+ ", 'longitude': " +document.getElementById('txtWellLong').value+ ", 'wellCapacity': " +document.getElementById('txtWellCapacity').value+ ", 'wellOutput': " +document.getElementById('txtWellOutput').value+ "}",
+                       contentType: "application/json; charset=utf-8",
+                       datatype: "json",
+                       async:false,
+                       success: function(msg){
+                           $('#txtWellGroup').val('');
+                           $('#txtWellName').val('')
+                           $('#txtWellType').val('');
+                           $('#txtWellLat').val('');
+                           $('#txtWellLng').val('');
+                           $('#txtWellCapacity').val('');
+                           $('#txtWellOutput').val('');
+                           $("#divResult").html("success");
+                       },
+                       error: function(e){
+                           $("#divResult").html("Something Wrong.");
+                       }
+                   });              
+               });
+            });
+
 	</script>
 </head>
 <body>
     <form id="form1" runat="server">
     <div>
-       
+
+
+       WellGroup: <asp:TextBox ID="txtWellGroup" runat="server" ClientIDMode="Static" Width="202px"></asp:TextBox>
+            <br />
+            <br />
+       WellName: <asp:TextBox ID="txtWellName" runat="server" ClientIDMode="Static" Width="210px"></asp:TextBox>
+            <br />
+            <br />           
+       WellType: <asp:TextBox ID="txtWellType" runat="server" ClientIDMode="Static" Width="202px"></asp:TextBox>
+            <br />
+            <br />           
+       Latitude: <asp:TextBox ID="txtWellLat" runat="server" ClientIDMode="Static" Width="202px"></asp:TextBox>
+            <br />
+            <br />           
+       Longitude: <asp:TextBox ID="txtWellLong" runat="server" ClientIDMode="Static" Width="202px"></asp:TextBox>
+            <br />
+            <br />           
+       WellCapacity: <asp:TextBox ID="txtWellCapacity" runat="server" ClientIDMode="Static" Width="202px"></asp:TextBox>
+            <br />
+            <br />           
+       WellOutput: <asp:TextBox ID="txtWellOutput" runat="server" ClientIDMode="Static" Width="202px"></asp:TextBox>
+            <br />
+            <br />
+
+
+
+
+
+        <asp:Button ID="Button1" runat="server" Text="Button" ClientIDMode="Static" />
+
     </div>
     </form>
 </body>
--- DBImport.aspx.cs
+++ DBImport.aspx.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Web;
 using System.Web.Services;
-using System.Data.SQLClient;
+using System.Data.SqlClient;
 using System.Web.Script.Services;
 using System.IO;
 using System.Xml.Serialization;
@@ -10,20 +10,23 @@ using System.Data;
 using System.Text;
 
 [WebService(Namespace = "http://team-avengineers.github.io/")]
-[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile_1)]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
 [System.Web.Script.Services.ScriptService]
 
-public class DBImport : System.Web.Services.WebService{
+//public class DBImport : System.Web.Services.WebService {
+public partial class _DBImport : System.Web.UI.Page {
 
-public Struct WellInfo{
-  int wellKey;
-  int wellGroup;
-  string wellName;
-  string wellType;
-  double latitude;
-  double longitude;
-  int wellCapacity;
-  int wellOutput;
+public struct WellInfo{
+  public int wellKey;
+  public int wellGroup;
+  public string wellName;
+  public string wellType;
+  public double latitude;
+  public double longitude;
+  public int wellCapacity;
+  public int wellOutput;
+
+
 }
 
 static List<WellInfo> wellInfoList = new List<WellInfo> {};
@@ -48,9 +51,8 @@ static void initialize_connection(){
 [WebMethod]
 static string add_new(int wellGroup, string wellname, string welltype, double latitude, double longitude, int wellCapacity, int wellOutput){
   
-   SQLConnection myConnection = new SQLConnection("userid = tconxnet_CScode; password = z2lafile6b2mq044; server = http://www.tconx.net/phpMyAdmin/; Trusted_Connection = yes; database = tconxnet_wellmap2; connection timeout = 30");
-   SQLCommand myCommand = new SQLCommand("INSERT into wells (wellGroup, wellName, wellType, lat, lng, wellCapacity, wellOutput")" +
-    "Values('wellgroup', 'wellname', 'welltype', latitude, longitude, wellCapacity, wellOutput)", myConnection);
+   SqlConnection myConnection = new SqlConnection("userid = tconxnet_CScode; password = z2lafile6b2mq044; server = http://www.tconx.net/phpMyAdmin/; Trusted_Connection = yes; database = tconxnet_wellmap2; connection timeout = 30");
+   SqlCommand myCommand = new SqlCommand("INSERT into wells Values('" + wellGroup + "', '" + wellname +"', '" + welltype + "', '" + latitude + "', '" + longitude + "', '" + wellCapacity + "', '" + wellOutput + "')", myConnection);
     try{
       myConnection.Open();
       myCommand.ExecuteNonQuery();
@@ -67,38 +69,39 @@ static string add_new(int wellGroup, string wellname, string welltype, double la
 [WebMethod]
 public List<WellInfo> read_data(){
      try{
-     SQLCommand command = new SQLCommand("select * from wells", myConnection);
+     SqlCommand command = new SqlCommand("select * from wells");
 	 DataSet ds = GetData(command);
 	 DataTable dt = ds.Tables[0];
 	 foreach(DataRow item in ds.Tables[0].Rows){
-		 wellInfo WI = new wellInfo();
-		 WI.wellKey = item["wellKey"].GetInt16();
-		 WI.wellGroup = item["wellGroup"].GetInt16();
+		 WellInfo WI = new WellInfo();
+		 WI.wellKey = Convert.ToInt16(item["wellKey"].ToString());
+		 WI.wellGroup = Convert.ToInt16(item["wellGroup"].ToString());
 		 WI.wellName = item["wellName"].ToString();
 		 WI.wellType = item["wellType"].ToString();
-		 WI.latitude = item["lat"].GetInt16();
-		 WI.longitude = item["lng"].GetInt16();
-		 WI.wellCapacity = item["wellCapacity"].GetInt16();
-		 WI.wellOutput = item["wellOutput"].GetInt16();
+		 WI.latitude = Convert.ToInt16(item["lat"].ToString());
+		 WI.longitude = Convert.ToInt16(item["lng"].ToString());
+		 WI.wellCapacity = Convert.ToInt16(item["wellCapacity"].ToString());
+		 WI.wellOutput = Convert.ToInt16(item["wellOutput"].ToString());
 		 wellInfoList.Add(WI);
 	 }
 	 return wellInfoList;
 
    }
-   Catch (Exception e){
+   catch (Exception e){
      Console.WriteLine(e.ToString());
+     return null;
    }
 }
 
 //used by readData to pull info from the database
-private static DataSet GetData(SQLCommand command){
+private static DataSet GetData(SqlCommand command){
 	string str = "userid = tconxnet_CScode; password = z2lafile6b2mq044; server = http://www.tconx.net/phpMyAdmin/; Trusted_Connection = yes; database = tconxnet_wellmap2; connection timeout = 30";
-	using (SQLConnection con = new SQLConnection(str)){
-		using (SQLDataAdaptor sda = new SQLDataAdaptor()){
+	using (SqlConnection con = new SqlConnection(str)){
+		using (SqlDataAdapter sda = new SqlDataAdapter()){
 			command.Connection = con;
 			sda.SelectCommand = command;
 			using (DataSet ds = new DataSet()){
-				sda.Fill(ds)
+                sda.Fill(ds);
 				return ds;
 			}
 		}
@@ -110,8 +113,8 @@ private static DataSet GetData(SQLCommand command){
 
 [WebMethod]
 public string update_well(int workingWellKey, int newWellGroup, string newWellname, string newWelltype, double newLatitude, double newLongitude, int newWellCapacity, int newWellOutput){
-  SQLConnection myConnection = new SQLConnection("userid = tconxnet_CScode; password = z2lafile6b2mq044; server = http://www.tconx.net/phpMyAdmin/; Trusted_Connection = yes; database = tconxnet_wellmap2; connection timeout = 30");
-  SQLCommand myCommand = new SQLCommand("UPDATE wells SET wellGroup = newWellGroup, wellName = newWellName, wellType = newWellType, lat = newLatitude, lng = newLongitude, wellCapacity = newWellCapacity, wellOutput = newWellOutput" +
+  SqlConnection myConnection = new SqlConnection("userid = tconxnet_CScode; password = z2lafile6b2mq044; server = http://www.tconx.net/phpMyAdmin/; Trusted_Connection = yes; database = tconxnet_wellmap2; connection timeout = 30");
+  SqlCommand myCommand = new SqlCommand("UPDATE wells SET wellGroup = newWellGroup, wellName = newWellName, wellType = newWellType, lat = newLatitude, lng = newLongitude, wellCapacity = newWellCapacity, wellOutput = newWellOutput" +
       "WHERE wellKey == workingWellKey", myConnection);
     try{
       myConnection.Open();
@@ -130,8 +133,8 @@ public string update_well(int workingWellKey, int newWellGroup, string newWellna
 
 [WebMethod]
 public string delete_well(int workingWellKey){
-    SQLConnection myConnection = new SQLConnection("userid = tconxnet_CScode; password = z2lafile6b2mq044; server = http://www.tconx.net/phpMyAdmin/; Trusted_Connection = yes; database = tconxnet_wellmap2; connection timeout = 30");
-    SQLCommand myCommand = new SQLCommand("Delete from wells WHERE wellKey = workingWellKey", myConnection);
+    SqlConnection myConnection = new SqlConnection("userid = tconxnet_CScode; password = z2lafile6b2mq044; server = http://www.tconx.net/phpMyAdmin/; Trusted_Connection = yes; database = tconxnet_wellmap2; connection timeout = 30");
+    SqlCommand myCommand = new SqlCommand("Delete from wells WHERE wellKey = workingWellKey", myConnection);
     try{
       myConnection.Open();
       myCommand.ExecuteNonQuery();
@@ -145,7 +148,7 @@ public string delete_well(int workingWellKey){
 }
 
 /*static void close_connection(){
-   SQLConnection myConnection = new SQLConnection("userid = tconxnet_CScode; password = z2lafile6b2mq044; server = http://www.tconx.net/phpMyAdmin/; Trusted_Connection = yes; database = tconxnet_wellmap2; connection timeout = 30");
+   SqlConnection myConnection = new SqlConnection("userid = tconxnet_CScode; password = z2lafile6b2mq044; server = http://www.tconx.net/phpMyAdmin/; Trusted_Connection = yes; database = tconxnet_wellmap2; connection timeout = 30");
    
   try{
   myConnection.Close();
