66a7b6247e4ffa3c0d00f57b6858c4ce49793f29
Thomas Condon <tconxi@gmail.com>
2015-03-04 22:45:34 -0700

    fix

--- JS/controller.js
+++ JS/controller.js
@@ -236,7 +236,7 @@ controller.prototype = {
 		
 	},
 	
-	function processData(alltext){
+	processData:function(alltext){
 		var allTextLines = allText.split(/\r\n|\n/);
 		var headers = allTextLines[0].split(',');
 		var lines = [];
@@ -251,8 +251,7 @@ controller.prototype = {
 				lines.push(tarr);
 			}
 		}
-	}
-}
+	},
 	
 	// TODO:
 	// Saving and Loading
