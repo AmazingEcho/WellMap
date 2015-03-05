31e57f8663302e8c8b8a1576ac571f2f1a4e07e2
Jennifer <jejaafar@ucalgary.ca>
2015-03-04 22:41:49 -0700

    fix

--- JS/controller.js
+++ JS/controller.js
@@ -236,23 +236,26 @@ controller.prototype = {
 		
 	},
 	
-	processData : function(alltext){
+	processData : function(alltext)
+	{
 		var allTextLines = allText.split(/\r\n|\n/);
 		var headers = allTextLines[0].split(',');
 		var lines = [];
 		
-		for(var i=1; i<allTextLines.length; i++){
+		for(var i=1; i<allTextLines.length; i++)
+		{
 			var data = allTextLines[i].split(',');
-			if(data.length == headers.length){
+			if(data.length == headers.length)
+			{
 			var tarr = [];
-			for(var j=0; j<headers.length; j++){
+			for(var j=0; j<headers.length; j++)
+				{
 				tarr.push(headers[j]+":"+data[j]);
 				}
 				lines.push(tarr);
 			}
 		}
-	}
-}
+	},
 	
 	// TODO:
 	// Saving and Loading
