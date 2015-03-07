32781380f5ce401f6c5cb88ad08ed68a6ea978e2
pkusoglu <pkusoglu@gmail.com>
2015-03-07 13:39:29 -0700

    icon fix

--- JS/ui.js
+++ JS/ui.js
@@ -499,7 +499,8 @@ $("#inputNameField").each(function ()
 				the_controller.Gmap.addMarker({
 							lat: points[j][i].lat,
 							lng: points[j][i].lng,
-							title: points[j][i].well_name
+							title: points[j][i].well_name,
+							icon: "markers/icon1.png"
 				});
 			}
 		}
